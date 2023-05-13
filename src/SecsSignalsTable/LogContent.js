6e47587cb70d08ba3372b624bc407f1137d590be
i-shuan <cjy931246@gmail.com>
2023-05-13 12:16:42 +0800

    Verify if SXFY appears in S7FY's first line

--- src/SecsSignalsTable/LogContent.js
+++ src/SecsSignalsTable/LogContent.js
@@ -8,7 +8,7 @@ const LogContent = () => {
   }, []);
 
   const fetchLogData = async () => {
-    const response = await fetch('/ABCDE.log');
+    const response = await fetch('/TAPSECS.log');
     const data = await response.text();
     setLogData(maskS7FYContent(data));
   }
@@ -19,45 +19,45 @@ const LogContent = () => {
     let lastS7FY = null;
     let result = [];
     let countMaskLine = 0;
-    var timeStampRegExp = /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}/;
+    const timeStampRegExp = /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}/;
+
     for (let i = 0; i < logLines.length; i++) {
-      if (logLines[i].includes('S7F')) {               
-        isMasked = true;       
-        const s7fMatch = logLines[i].match(/S7F\d{1,2}/);
-        
-        const time = logLines[i].match(timeStampRegExp);
-
-        if(s7fMatch !== lastS7FY){
-          /* Different S7FY => Reset Count */   
-          countMaskLine = 0;
-          lastS7FY = s7fMatch
-        }
-        result.push(`${time[0]} - ${s7fMatch} ***********`);
-        continue;
-
-      } else if (timeStampRegExp.test(logLines[i])) {
-       
-        /* lastS7FY countent*/
-        if(isMasked && countMaskLine === 0){
-          const time = logLines[i].match(/\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}/);
-          result.push(`${time[0]} - ***** Content ******`);
-        }
-        else{
-          isMasked = false;
-          lastS7FY = null
-          countMaskLine = 0;
-        }
-        countMaskLine = isMasked? countMaskLine+1:countMaskLine;
-      }
       
-      if(!isMasked){
-        result.push(logLines[i]);
-      }
-     
+      
+
+        if (logLines[i].includes('S7F')) {               
+            isMasked = true;       
+            const s7fMatch = logLines[i].match(/S7F\d{1,2}/);
+            const time = logLines[i].match(timeStampRegExp);
+            if(s7fMatch !== lastS7FY){
+                countMaskLine = 0;
+                lastS7FY = s7fMatch;
+            }
+            result.push(`${time[0]} - ${s7fMatch} ***********`);
+            continue;
+
+        } else if (logLines[i].match(/S[123456890]F/)) {
+            if(isMasked && countMaskLine!==0){
+              isMasked = false;
+            }
+           
+        }
+
+        if(isMasked && timeStampRegExp.test(logLines[i])){
+
+            const time = logLines[i].match(timeStampRegExp);
+            result.push(`${time[0]} - ******Content*****`);     
+        }
+        else if(!isMasked){
+          result.push(logLines[i]);
+        }
+
+        countMaskLine = isMasked? countMaskLine+1: countMaskLine;
     }
-  
+
     return result.join('\n');
-  };
+};
+
 
   return (
     <div>
